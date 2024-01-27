ee1b9c35f4266a5753c004dadc69c8f922b15c14
Adelpozo04 <addelp01@ucm.es>
2024-01-27 01:59:27 +0100

    Parece que funciona la camara y el input

---
+++ GlobalProject/Assets/Scripts/PlayerMovementAPH.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementAPH : MonoBehaviour
+{
+
+
+    #region properties
+
+
+    private CharacterController controller;
+
+    private int dirX = 0;
+
+    private int dirY = 0;
+
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        controller.GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+
+        
+    }
+}
