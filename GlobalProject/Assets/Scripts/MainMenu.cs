b824a3b297946a8f55eb0eb8ff8fc447c7acbc9f
Vivu24 <javivu2.12004@gmail.com>
2024-01-28 00:30:55 +0100

    Funciona gente, falta quitar el input

--- GlobalProject/Assets/Scripts/MainMenu.cs
+++ GlobalProject/Assets/Scripts/MainMenu.cs
@@ -6,43 +6,22 @@ using Cinemachine;
 public class MainMenu : MonoBehaviour
 {
     public GameObject menuUI;
-    public CinemachineSmoothPath smoothPath;
-    public CinemachineVirtualCamera virtualCamera;
+    public Animator animator;
 
     void Start()
     {
         menuUI.SetActive(true);
-
-        if (smoothPath != null)
-        {
-            smoothPath.gameObject.SetActive(false);
-        }
-
-        if (virtualCamera != null)
-        {
-            // Desactiva manualmente la cinemática de la cámara al inicio
-            virtualCamera.enabled = false;
-        }
     }
 
     public void PlayGame()
     {
         Debug.Log("play");
 
+        animator.SetBool("Play", true);
+
         // Desactiva el menú
         menuUI.SetActive(false);
 
-        // Activa tanto el smoothPath como la virtualCamera al presionar "Play Game"
-        if (smoothPath != null)
-        {
-            smoothPath.gameObject.SetActive(true);
-        }
-
-        if (virtualCamera != null)
-        {
-            // Activa manualmente la cinemática de la cámara al presionar "Play Game"
-            virtualCamera.enabled = true;
-        }
     }
 
     public void QuitGame()
