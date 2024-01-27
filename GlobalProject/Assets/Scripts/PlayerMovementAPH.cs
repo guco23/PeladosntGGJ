ef7658a25f725fc20c13214df3ea93aaf3d2d44e
Adelpozo04 <addelp01@ucm.es>
2024-01-27 04:43:12 +0100

    Ya hay salto

--- GlobalProject/Assets/Scripts/PlayerMovementAPH.cs
+++ GlobalProject/Assets/Scripts/PlayerMovementAPH.cs
@@ -1,3 +1,4 @@
+using Palmmedia.ReportGenerator.Core.Parser.Filtering;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,15 +10,14 @@ public class PlayerMovementAPH : MonoBehaviour
 
     #region parameters
 
-
     [SerializeField] private float speed;
-
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
 
     #endregion
 
 
     #region references
-
 
 
 
@@ -31,21 +31,54 @@ public class PlayerMovementAPH : MonoBehaviour
 
     private Vector2 myDir;
 
-    private int dirX = 0;
-
-    private int dirY = 0;
-
     private Camera mainCamera;
+
+    private float actualVelocity;
+
+    private Vector3 move;
+
 
 
     #endregion
 
 
+    private void ApplyGravity()
+    {
+
+        move.y = actualVelocity;
+
+        if (controller.isGrounded && move.y < 0)
+        {
+
+            actualVelocity = -1;
+
+        }
+        else
+        {
+            actualVelocity -= gravity * Time.deltaTime;
+        }
+
+       
+    }
 
     public void DirMovement(InputAction.CallbackContext context)
     {
 
         myDir = context.ReadValue<Vector2>();
+
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log(controller.isGrounded);
+
+        if (controller.isGrounded && context.started)
+        {
+
+            actualVelocity = jumpForce;
+
+        }
+
 
     }
 
@@ -64,13 +97,15 @@ public class PlayerMovementAPH : MonoBehaviour
     void Update()
     {
 
-        Vector3 move = myDir.y * mainCamera.transform.forward + myDir.x * mainCamera.transform.right;
+        move = (myDir.y * mainCamera.transform.forward + myDir.x * mainCamera.transform.right) * speed;
 
-        move.y = 0;
+        ApplyGravity();
 
-        Debug.Log(move);
+        controller.Move(move * Time.deltaTime);
 
-        controller.Move(move * Time.deltaTime * speed);
         
+
+        
+
     }
 }
