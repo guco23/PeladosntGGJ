63ed6ce75a1990ed6211096e625ed57f22d54cb8
Vivu24 <javivu2.12004@gmail.com>
2024-01-27 13:20:50 +0100

    Musica Cross Goat

--- GlobalProject/Assets/Scripts/Testing Music/MusicComponent.cs
+++ GlobalProject/Assets/Scripts/Testing Music/MusicComponent.cs
@@ -7,10 +7,12 @@ public class MusicComponent : MonoBehaviour
 {
     [SerializeField] private bool _changeMusic = false;
 
-    private bool _playingMenu = true;
+    private bool _playingPlayableMusic = false;
 
     [SerializeField] private AudioSource _menuSource;
     [SerializeField] private AudioSource _playableSource;
+
+    [SerializeField] private float _fadeTime;
 
 
     // Start is called before the first frame update
@@ -22,8 +24,13 @@ public class MusicComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_changeMusic && _playingMenu)
+        if (_changeMusic && _menuSource.volume >= 0)
         {
+            if (!_playingPlayableMusic)
+            {
+                _playableSource.Play();
+                _playingPlayableMusic = true;
+            }
             Debug.Log("Cambio de Música");
             ChangeToPlayableState();
         }
@@ -31,8 +38,8 @@ public class MusicComponent : MonoBehaviour
 
     private void ChangeToPlayableState()
     {
-        _playingMenu = false;
-        _menuSource.volume = 0;
-        _playableSource.Play();
+        _menuSource.volume = Mathf.Lerp(_menuSource.volume, 0, Time.deltaTime * _fadeTime);
+        _playableSource.volume = Mathf.Lerp(_playableSource.volume, 1, Time.deltaTime * _fadeTime);
+
     }
 }
