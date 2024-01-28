886757e2c8be1ed42250067fcb0fb6d05f291f30
Adelpozo04 <addelp01@ucm.es>
2024-01-28 15:42:15 +0100

    Nueva Build

--- GlobalProject/Assets/Scripts/GameManager.cs
+++ GlobalProject/Assets/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : MonoBehaviour
 
     public void ChangeCursorState()
     {
-        Cursor.visible = !Cursor.visible;
+       //Cursor.visible = !Cursor.visible;
     }
 
     public void updateOrdersList(List<string> list)
